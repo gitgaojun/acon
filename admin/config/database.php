052d16a48d42d7df1a4cdd8447f5ad7d0b9eae28
gaojun610 <gaojun610gj@gmail.com>
2015-03-09 16:55:37 +0800

    修改数据库配置,之前的报错是

--- admin/config/database.php
+++ admin/config/database.php
@@ -48,21 +48,21 @@
 $active_group = 'acon';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '127.0.0.1';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'acon';
-$db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = TRUE;
-$db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
-$db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
+$db['acon']['hostname'] = '127.0.0.1';
+$db['acon']['username'] = 'root';
+$db['acon']['password'] = 'root';
+$db['acon']['database'] = 'acon';
+$db['acon']['dbdriver'] = 'mysql';
+$db['acon']['dbprefix'] = '';
+$db['acon']['pconnect'] = TRUE;
+$db['acon']['db_debug'] = TRUE;
+$db['acon']['cache_on'] = FALSE;
+$db['acon']['cachedir'] = '';
+$db['acon']['char_set'] = 'utf8';
+$db['acon']['dbcollat'] = 'utf8_general_ci';
+$db['acon']['swap_pre'] = '';
+$db['acon']['autoinit'] = TRUE;
+$db['acon']['stricton'] = FALSE;
 
 
 /* End of file database.php */
