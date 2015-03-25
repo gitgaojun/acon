3bd6d76bdaf2ca397fc3e817212015aca1f7cde9
gaojun610 <gaojun610gj@gmail.com>
2015-03-25 11:05:07 +0800

    添加菜单栏

---
+++ admin/controllers/sys_menu.php
@@ -0,0 +1,38 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: jun90610@gmail.com
+ * Date: 2015/3/25
+ * Time: 9:42
+ */
+
+    /**
+     * Class sys_menu
+     * 系统菜单栏
+     */
+    class sys_menu extends AD_Controller
+    {
+
+        function __construct()
+        {
+            parent::__construct();
+        }
+
+        /**
+         * 菜单列表,读取缓存文件
+         */
+        public function index()
+        {
+            $data['title'] = "系统菜单栏";
+
+
+
+            $this->load->vars($data);
+            $this->load->view("sys_menu");
+
+        }
+
+
+
+    }
